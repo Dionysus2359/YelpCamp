4c596959baa587e0cd6150c20523edf41d6900e9
Dionysus2359 <omtiwari2359@gmail.com>
2025-09-11 16:11:32 +0530

    fixed some bugs

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Oh no, Something went wrong!"
     res.status(statusCode).render('error', { err });
 })
-
-app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`SERVING ON PORT ${port}`);
 })
